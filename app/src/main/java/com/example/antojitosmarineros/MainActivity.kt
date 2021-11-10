df76273ccc8d693a83df51ac1993e4a7b24bd14d
JACQUELINE TORRES BENG <16171509@itculiacan.edu.mx>
2021-11-10 16:43:06 -0700

    1

--- app/src/main/java/com/example/antojitosmarineros/MainActivity.kt
+++ app/src/main/java/com/example/antojitosmarineros/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.antojitosmarineros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    fun inicio(view :View)
+    {
+        //val abrir = intent(this, Display::com)
+    }
+
 }
